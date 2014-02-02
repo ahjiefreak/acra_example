81ad4ef8164d031eec06df0865980582fd3e0511
Medo <dimoski.ivan@gmail.com>
2014-02-02 18:46:52 +0100

    Initial commit

---
+++ acra/src/main/java/com/example/acra/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.acra;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_crash).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String crashString = null;
+                crashString.length();
+            }
+        });
+    }
+}
